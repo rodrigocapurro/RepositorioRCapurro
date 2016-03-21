181ea6b378caf05d9aeb99741b2c60b90f866c8e
rodrigocapurro <rodrigomartincapurro@hotmail.com>
2016-03-21 16:40:04 -0300

    Signed-off-by: rodrigocapurro <rodrigomartincapurro@hotmail.com>
    Se realiza el ultimo juego "Reflejos Daltonicos"

--- Trabajo Practico para juegos/TP1LAB4/TP1LAB4/TP lab4/jsReflejosDaltonicos1.js
+++ Trabajo Practico para juegos/TP1LAB4/TP1LAB4/TP lab4/jsReflejosDaltonicos1.js
@@ -4,18 +4,25 @@ mostrara el texto de un color entre los 6 posibles
  para que el jugador sepa que botón tocar .Al tocar 
  el botón correcto se informara cuanto tiempo tardo.
 */
-var ColorSecreto;
-var tiempoInicio;
+var ColorSecreto =  'Rojo';
+var tiempoInicio = 0;
 function comenzar()
 {
-
+	setInterval(function(){
+		tiempoInicio = tiempoInicio + 1;
+	},1000);
 	
+	document.getElementById('ColorElejido').value = ColorSecreto;
 
 }//FIN DE LA FUNCIÓN
 function Responder(colorParametro)
 {
-	
-	
+	if(colorParametro == ColorSecreto.toLowerCase()){
+		alert('Acertaste!!! \n\n tiempo de demora: ' + tiempoInicio + ' Segundos');
 
+	}
+	else{
+		alert('El color elejido es incorrecto, tiempo de demora: ' + tiempoInicio + ' Segundos');
+	}
 
 }//FIN DE LA FUNCIÓN
