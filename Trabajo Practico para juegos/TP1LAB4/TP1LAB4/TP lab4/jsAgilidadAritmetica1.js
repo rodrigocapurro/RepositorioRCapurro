e4ba6daa8e8297ab0a2af3ce08d01128840596de
rodrigocapurro <rodrigomartincapurro@hotmail.com>
2016-03-21 15:36:57 -0300

    Signed-off-by: rodrigocapurro <rodrigomartincapurro@hotmail.com>
    Se realiza el juego de agilidad aritmetica nro 1

--- Trabajo Practico para juegos/TP1LAB4/TP1LAB4/TP lab4/jsAgilidadAritmetica1.js
+++ Trabajo Practico para juegos/TP1LAB4/TP1LAB4/TP lab4/jsAgilidadAritmetica1.js
@@ -10,11 +10,55 @@ var respuesta;
 function comenzar()
 {
 
+	var primerNumero = (Math.random() * 10).toFixed(0);
+	var segundoNumero = (Math.random() * 10).toFixed(0);
 	
+	document.getElementById('PrimerNumero').value= primerNumero;
+	document.getElementById('SegundoNumero').value= segundoNumero;
+	var operacion = (Math.random() * 4).toFixed(0);
+	if(operacion==0){
+		operacion= 1;
+	}
 
+	var simboloOperacion = obtenerSimboloOperacion(operacion);
+
+	document.getElementById('Operador').value = simboloOperacion;
+
+	if(simboloOperacion == '/' && segundoNumero ==0){
+		respuesta = 'división por 0';
+	}
+	else{
+		respuesta = eval(primerNumero + ' ' + simboloOperacion + ' ' + segundoNumero);
+	}
 }//FIN DE LA FUNCIÓN
 function Responder()
 {
+	if(respuesta == document.getElementById('Respuesta').value){
 	
+	alert('Acertaste!!!');
+		
+	}
+
+	else if(respuesta == 'división por 0'){
+		alert('refresque la pagina para actualizar los numero a calcular, ya que no se puede dividir por 0');
+	}
+	else if(respuesta != document.getElementById('Respuesta').value){
+		alert('El valor ingresado es incorrecto')
+	}
 
 }//FIN DE LA FUNCIÓN
+
+function obtenerSimboloOperacion(operacion){
+	if(operacion==1){
+		return '+';
+	}
+	else if(operacion==2){
+		return '-';
+	}
+	else if(operacion==3){
+		return '*';
+	}
+	else if(operacion==4){
+		return '/';
+	}
+}
