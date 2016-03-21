564ef6796f7b18c91851ec48b44fa97be5c7f5ac
rodrigocapurro <rodrigomartincapurro@hotmail.com>
2016-03-21 15:56:25 -0300

    Signed-off-by: rodrigocapurro <rodrigomartincapurro@hotmail.com>

--- Trabajo Practico para juegos/TP1LAB4/TP1LAB4/TP lab4/jsAgilidadAritmetica2.js
+++ Trabajo Practico para juegos/TP1LAB4/TP1LAB4/TP lab4/jsAgilidadAritmetica2.js
@@ -4,14 +4,81 @@ se agregara un temporizador que
  el juego  de no ser ingresado el resultado 
  correcto en ese lapso de tiempo. */
 var respuesta;
-var temporizador;
+var temporizador=0;
 function comenzar()
 {
+
+	var primerNumero = (Math.random() * 10).toFixed(0);
+	var segundoNumero = (Math.random() * 10).toFixed(0);
 	
+	document.getElementById('PrimerNumero').value= primerNumero;
+	document.getElementById('SegundoNumero').value= segundoNumero;
+	var operacion = (Math.random() * 4).toFixed(0);
+	if(operacion==0){
+		operacion= 1;
+	}
+
+	var simboloOperacion = obtenerSimboloOperacion(operacion);
+
+	document.getElementById('Operador').value = simboloOperacion;
+
+	if(simboloOperacion == '/' && segundoNumero ==0){
+		respuesta = 'división por 0';
+	}
+	else{
+		respuesta = eval(primerNumero + ' ' + simboloOperacion + ' ' + segundoNumero);
+	}
+	
+	setTimeout(function(){
+		
+		if(temporizador = 4){
+		
+			alert('tiempo excedido, resultado de la operacion: ' + respuesta);
+			location.reload();
+		
+		}
+	
+		temporizador = temporizador + 1;
+	
+	},4000);
+
 }//FIN DE LA FUNCIÓN
 function Responder()
 {
+	if(respuesta == document.getElementById('Respuesta').value){
 	
+	alert('Acertaste!!!');
+		
+	}
 
+	else if(respuesta == 'división por 0'){
+		alert('refresque la pagina para actualizar los numero a calcular, ya que no se puede dividir por 0');
+	}
+	else if(respuesta != document.getElementById('Respuesta').value){
+		alert('El valor ingresado es incorrecto')
+	}
 
 }//FIN DE LA FUNCIÓN
+
+function obtenerSimboloOperacion(operacion){
+	if(operacion==1){
+		return '+';
+	}
+	else if(operacion==2){
+		return '-';
+	}
+	else if(operacion==3){
+		return '*';
+	}
+	else if(operacion==4){
+		return '/';
+	}
+}
+
+function timeout(){
+
+	setTimeout(function(){
+
+	});
+
+}
